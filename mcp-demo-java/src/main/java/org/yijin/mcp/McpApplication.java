cce2176a50f6461b5b59f7246031f065ebe5e34a
yijin840 <2900086967@qq.com>
2025-10-31 15:11:30 +0800

    添加Java17配置

---
+++ mcp-demo-java/src/main/java/org/yijin/mcp/McpApplication.java
@@ -0,0 +1,18 @@
+package org.yijin.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : yijin
+ * @email : yijin840@gmail.com
+ * @created : 10/31/25 2:55 PM
+ **/
+@SpringBootApplication
+public class McpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpApplication.class, args);
+    }
+
+}
---
+++ mcp-demo-java/src/main/java/org/yijin/mcp/client/McpClientDemo.java
@@ -0,0 +1,9 @@
+package org.yijin.mcp.client;
+
+/**
+ * @author : yijin
+ * @email : yijin840@gmail.com
+ * @created : 10/31/25 11:40 AM
+ **/
+public class McpClientDemo {
+}
